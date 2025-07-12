a41de120587203d104b648f7b33e1c05ee376490
Timothy Baker <35656692+silence48@users.noreply.github.com>
2025-07-12 20:51:44 +0000

    use dynamic paths for pnpm and node

--- setup-systemd.sh
+++ setup-systemd.sh
@@ -4,6 +4,8 @@ set -e
 SERVICE_FILE="/etc/systemd/system/stellaratlas.service"
 WORKDIR="$(pwd)"
 USER="$(whoami)"
+NODE_BIN="$(dirname $(which node))"
+PNPM_BIN="$(which pnpm)"
 
 sudo tee "$SERVICE_FILE" > /dev/null <<EOF
 [Unit]
@@ -13,11 +15,12 @@ After=network.target
 [Service]
 Type=simple
 WorkingDirectory=$WORKDIR
-ExecStart=pnpm start:all
+ExecStart=$PNPM_BIN start:all
 Restart=always
 RestartSec=5
 User=$USER
 Environment=NODE_ENV=production
+Environment=PATH=$NODE_BIN:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 [Install]
 WantedBy=multi-user.target
